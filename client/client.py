da7a430aeb965228ef8a4ff2e21f02f03e3e4698
Charles Houser <cjohouser@gmail.com>
2020-01-30 14:42:24 -0800

    newclient.py with resizability

--- client/client.py
+++ client/client.py
@@ -3,20 +3,21 @@
 
 from argparse import ArgumentParser
 from collections import deque
-from curses import newpad, wrapper
+from curses import KEY_RESIZE, newpad, wrapper
 from math import ceil
 from select import select
 from socket import socket
 from sys import stdin
 
 
-def drawBorders(stdscr, y, x):
+def drawBorders(stdscr, terminalHeight, terminalWidth, textPadHeight):
+    separatorHeight = terminalHeight - textPadHeight - 2
     stdscr.clear()
     stdscr.border()
-    stdscr.addch(y - 5, 0,'├')
-    for i in range(1, x):
-        stdscr.addch(y - 5, i, '─')
-    stdscr.addch(y - 5, x - 1, '┤')
+    stdscr.addch(separatorHeight, 0,'├')
+    for i in range(1, terminalWidth):
+        stdscr.addch(separatorHeight, i, '─')
+    stdscr.addch(separatorHeight, terminalWidth - 1, '┤')
     stdscr.refresh()
 
 
@@ -25,32 +26,30 @@ def main(stdscr):
     sock.connect((args.host, args.port))
     sock.sendall('/setname {}\n'.format(args.name).encode())
 
-    termy, termx = stdscr.getmaxyx()
-    drawBorders(stdscr, termy, termx)
-    chatPad = newpad(termy - 6, termx - 2)
-    chatPad.refresh(0, 0, 1, 1, termy - 6, termx - 2)
-    textPad = newpad(3, 78)
-    textPad.refresh(0, 0, 20, 1, 23, 78)
+    terminalHeight, terminalWidth = stdscr.getmaxyx()
+    textPadHeight = 5
+
+    drawBorders(stdscr, terminalHeight, terminalWidth, textPadHeight)
+
+    chatPadHeight = terminalHeight - textPadHeight - 3
+    chatPadWidth = terminalWidth - 2
+    chatPad = newpad(chatPadHeight, chatPadWidth)
+    chatPad.refresh(0, 0, 1, 1, chatPadHeight, chatPadWidth)
+
+    textPadWidth = terminalWidth - 2
+    textPad = newpad(textPadHeight, textPadWidth)
+    textPad.refresh(0, 0, terminalHeight - textPadHeight - 1, 1,
+                    terminalHeight - 1, textPadWidth)
     
     chatPadCursorY = 0
     sendString = ''
-
     inbound = [sock, stdin]
     outbound = [sock]
     messageQueue = deque()
     while True:
         readable, writable, exceptional = select(inbound, outbound, [])
         for fd in readable:
-            if fd == sock:
-                decodedRecv = sock.recv(256).decode()
-                rowCount = int(ceil(len(decodedRecv) / 78))
-                if rowCount + chatPadCursorY > termy - 6:
-                    chatPadCursorY = 0
-                    chatPad.clear()
-                chatPad.addstr(chatPadCursorY, 0, decodedRecv)
-                chatPadCursorY += rowCount
-                chatPad.refresh(0, 0, 1, 1, termy - 6, termx - 2)
-            elif fd == stdin:
+            if fd == stdin:
                 c = stdin.read(1)
                 if c == '\r':
                     messageQueue.append(sendString)
@@ -64,9 +63,20 @@ def main(stdscr):
                 else:
                     sendString += c
                 textPad.addstr(0, 0, sendString)
-                textPad.refresh(0, 0, 20, 1, 23, 78)
+                textPad.refresh(0, 0, terminalHeight - textPadHeight - 1, 1,
+                                terminalHeight - 1, textPadWidth)
+            elif fd == sock:
+                decodedRecv = sock.recv(256).decode()
+                rowCount = int(ceil(len(decodedRecv) / 78))
+                if rowCount + chatPadCursorY >= chatPadHeight:
+                    chatPadCursorY = 0
+                    chatPad.clear()
+                chatPad.addstr(chatPadCursorY, 0, decodedRecv)
+                chatPadCursorY += rowCount
+                chatPad.refresh(0, 0, 1, 1, terminalHeight - 6,
+                                terminalWidth - 2)
         for fd in writable:
-            if messageQueue:
+            if fd == sock and messageQueue:
                 fd.sendall(messageQueue.popleft().encode())
         
     
